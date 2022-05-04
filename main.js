7ddb47f5e29f092ee9c3e9d8a7a2c8070c4dfeb6
fkrein1 <felipekrein@gmail.com>
2022-05-04 14:03:28 -0300

    dia 03 parcial

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const btnMenuOpen = document.querySelector('.open-menu')
 const btnMenuClose = document.querySelector('.close-menu')
+const menuLinks = document.querySelectorAll('.menu-links li')
+
 
 function onScroll() {
   const nav = document.querySelector('#navigation')
@@ -20,3 +22,16 @@ function closeMenu() {
 document.addEventListener('scroll', onScroll)
 btnMenuOpen.addEventListener('click', openMenu)
 btnMenuClose.addEventListener('click', closeMenu)
+menuLinks.forEach(element => element.addEventListener('click', closeMenu))
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .card`)
