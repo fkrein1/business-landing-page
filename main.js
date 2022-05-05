7af3fe004072d3460d67d5f6da67a528de10a8c0
fkrein1 <felipekrein@gmail.com>
2022-05-05 13:06:15 -0300

    aula  04

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const btnMenuOpen = document.querySelector('.open-menu')
 const btnMenuClose = document.querySelector('.close-menu')
 const menuLinks = document.querySelectorAll('.menu-links li')
+const btnWhatsapp = document.querySelectorAll('button.whatsapp')
 
-
-function onScroll() {
+function changeHeaderColor() {
   const nav = document.querySelector('#navigation')
   if (scrollY > 0) nav.classList.add('scroll')
   else nav.classList.remove('scroll')
+}
+
+function showTopButton() {
+  const topBtn = document.querySelector('#back-top-button')
+  if (scrollY > 400) topBtn.classList.add('show')
+  else topBtn.classList.remove('show')
+}
+
+function onScroll() {
+  changeHeaderColor();
+  showTopButton()
 }
 
 function openMenu() {
@@ -19,10 +30,15 @@ function closeMenu() {
   body.classList.remove('menu-expanded')
 }
 
+function toWhatsapp() {
+  window.open('https://wa.me/5511937004490', '_blank');
+}
+
 document.addEventListener('scroll', onScroll)
 btnMenuOpen.addEventListener('click', openMenu)
 btnMenuClose.addEventListener('click', closeMenu)
 menuLinks.forEach(element => element.addEventListener('click', closeMenu))
+btnWhatsapp.forEach(element => element.addEventListener('click', toWhatsapp))
 
 ScrollReveal({
   origin: 'top',
